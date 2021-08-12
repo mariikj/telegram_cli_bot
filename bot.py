3289e0ab53193b9c242a334f1c827905d08e9173
maryam kashani jou <kashanijoumaryam@gmail.com>
2021-08-12 22:10:49 +0430

    adding a channel existance check function to prevent adding duplicate channel name

--- bot.py
+++ bot.py
@@ -6,6 +6,17 @@ app = Client("my_account",config_file="config.ini")
 
 channel_lists = []
 
+def channel_exists(channel_name):
+
+    with open("channel_lists.csv", mode = 'r')as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            if channel_name == line[0]:
+                return True
+
+    return False  
+
+
 @app.on_message(filters.channel)
 async def project_board_messages(client, message):
     print(message.chat.username)
@@ -13,11 +24,13 @@ async def project_board_messages(client, message):
         await app.send_message("@Marii_kj", message.text)
         await app.send_message("@Marii_kj", "from: @" + str(message.chat.username))
 
+
 @app.on_message(filters.me)
 async def add_channel(client, message):
     if message.text.split()[0] == "add_channel":
         with open("channel_lists.csv", mode='a') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([message.text.split()[1]])
-            
+            if channel_exists(message.text.split()[1]) == False:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow([message.text.split()[1]])
+
 app.run()
