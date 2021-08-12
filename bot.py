f8525dd40c3e0a13dddf8321d4515bf4b1609e54
maryam kashani jou <kashanijoumaryam@gmail.com>
2021-08-12 21:10:08 +0430

    adding '@' before channel username to make it a real username

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 from pyrogram import Client, filters
+from pyrogram.raw import functions
+import csv
 
 app = Client("my_account",config_file="config.ini")
+
+channel_lists = []
 
 @app.on_message(filters.channel)
 async def project_board_messages(client, message):
     print(message.chat.username)
     if message.chat.username == 'project_board':
         await app.send_message("@Marii_kj", message.text)
-        await app.send_message("@Marii_kj", "from: " + str(message.chat.username))
+        await app.send_message("@Marii_kj", "from: @" + str(message.chat.username))
 
 
 @app.on_message(filters.private)
