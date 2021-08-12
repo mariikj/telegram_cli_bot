cf29e88d2894b9d0e8a9fd165afce5efa30d3ebd
maryam kashani jou <kashanijoumaryam@gmail.com>
2021-08-12 22:24:25 +0430

    declaring 2 variable for more Readability

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def channel_exists(channel_name):
         for line in csv_reader:
             if channel_name == line[0]:
                 return True
-
+    
     return False  
 
 @app.on_message(filters.channel)
@@ -27,10 +27,14 @@ async def project_board_messages(client, message):
 #adding new channel with "add_channel 'channel name'" message that i personally should send 
 @app.on_message(filters.me)
 async def add_channel(client, message):
-    if message.text.split()[0] == "add_channel":
+    
+    command = message.text.split()[0]
+    channel_name = message.text.split()[1]
+
+    if command == "add_channel":
         with open("channel_lists.csv", mode='a') as csv_file:
-            if channel_exists(message.text.split()[1]) == False:
+            if channel_exists(channel_name) == False:
                 csv_writer = csv.writer(csv_file)
-                csv_writer.writerow([message.text.split()[1]])
+                csv_writer.writerow([channel_name])
 
 app.run()
