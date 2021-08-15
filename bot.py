e17de9a58f1ad38063005b912de39ee7dc8ae681
mariikj <mmalakeh7@gmail.com>
2021-08-15 12:18:47 +0430

    hi

--- bot.py
+++ bot.py
@@ -2,16 +2,19 @@ from pyrogram import Client, filters
 from pyrogram.raw import functions
 import csv
 
-app = Client("my_account",config_file="config.ini")
+app = Client("my_account", config_file="config.ini")
+
+
+
 
 def get_channel_lists():
     channel_names = []
-    with open("channel_lists.csv", mode = 'r')as csv_file:
+    with open("channel_lists.csv", mode='r')as csv_file:
         csv_reader = csv.reader(csv_file)
         for line in csv_reader:
             channel_names.append(line[0])
         return channel_names
-            
+
 
 @app.on_message(filters.channel)
 async def project_board_messages(client, message):
@@ -20,21 +23,25 @@ async def project_board_messages(client, message):
         await app.send_message("@Marii_kj", message.text)
         await app.send_message("@Marii_kj", "from: @" + str(message.chat.username))
 
-#checking if a channel is already added 
+# checking if a channel is already added
+
+
 def channel_exists(channel_name):
 
-    with open("channel_lists.csv", mode = 'r')as csv_file:
+    with open("channel_lists.csv", mode='r')as csv_file:
         csv_reader = csv.reader(csv_file)
         for line in csv_reader:
             if channel_name == line[0]:
                 return True
     csv_file.close()
-    return False  
+    return False
 
-#adding new channel with "add_channel 'channel name'" message that i personally should send 
+# adding new channel with "add_channel 'channel name'" message that i personally should send
+
+
 @app.on_message(filters.me)
 async def add_channel(client, message):
-    
+
     command = message.text.split()[0]
     channel_name = message.text.split()[1]
 
